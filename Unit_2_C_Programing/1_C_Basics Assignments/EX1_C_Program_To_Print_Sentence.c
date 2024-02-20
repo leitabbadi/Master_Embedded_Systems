b418e9c110c8ce0c93df5cc3f8a633afb82aa75c
Leit Abbadi <158479861+leitabbadi@users.noreply.github.com>
2024-02-20 03:34:36 -0800

    Update EX1_C_Program_To_Print_Sentence.c

--- Unit_2_C_Programing/1_C_Basics Assignments/EX1_C_Program_To_Print_Sentence.c
+++ Unit_2_C_Programing/1_C_Basics Assignments/EX1_C_Program_To_Print_Sentence.c
@@ -1,1 +1,20 @@
+/*
+ ====================================================Home_work-1=======================================================
+Name        : EX1_C_Program_To_Print_Sentence.c
+Author      : Leit Abbadi
+Created on	: Feb 19, 2024
+Description : Unit 2 C-Programming >> Assignment 1 C-Basic >> EX1 C Program To Print Sentence using Ansi-style (c99)
+========================================================================================================================
+ */
+ 
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      printf("C Programming Learn In Depth");
+      return 0;
+}
+
+
 

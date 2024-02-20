03fd752586b4f9eacef62c8507b9cfae8fbfde09
Leit Abbadi <158479861+leitabbadi@users.noreply.github.com>
2024-02-20 03:41:08 -0800

    Update EX2_C_Program_To_Print_an_Integer_Entered_by_a_User.c

--- Unit_2_C_Programing/1_C_Basics Assignments/EX2_C_Program_To_Print_a_Integer_Entered_by_a_User.c
+++ Unit_2_C_Programing/1_C_Basics Assignments/EX2_C_Program_To_Print_a_Integer_Entered_by_a_User.c
@@ -1,1 +1,27 @@
+/*
+ ====================================================Home_work-1=======================================================================
+Name        : EX2_C_Program_To_Print_Sentence.c
+Author      : Leit Abbadi
+Created on	: Feb 19, 2024
+Description : Unit 2 C-Programming >> Assignment 1 C-Basic >> EX2 C Program To Print a Integer Entered by a User using Ansi-style (c99)
+ =======================================================================================================================================
+ */
+ 
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	
+	    int X;
+
+	    printf("Enter an integer:");
+	    fflush(stdout);
+	    scanf("%d",&X);
+	    printf("You entered= %d", X);
+
+	    return 0;
+}
+
+
 

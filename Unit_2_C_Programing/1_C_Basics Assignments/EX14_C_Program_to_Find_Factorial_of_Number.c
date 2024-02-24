dcd98af79e415caf31dbd448e2f8dc4ed33a4d55
Leit Abbadi <158479861+leitabbadi@users.noreply.github.com>
2024-02-24 05:34:16 -0800

    Create EX14_C_Program_to_Find_Factorial_of_Number.c

---
+++ Unit_2_C_Programing/1_C_Basics Assignments/EX14_C_Program_to_Find_Factorial_of_Number.c
@@ -0,0 +1,32 @@
+/*
+ ===================================Home_work-2========================================
+ Author      : Leit Abbadi
+ Created on	 : Feb 22, 2024
+ Description : Unit 2 C-Progtamming >> Assignment 2 C-Basic >> EX14 C program
+              to Find Factorial! of Numbers using Ansi-style (c99)
+ ======================================================================================
+ */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+	int x,i,sum=1;
+
+	printf("Enter an integer:");
+	fflush(stdout);
+	scanf("%d",&x);
+
+
+	for(i=1;i<=x;i++)
+	{
+		sum *= i;
+	}
+	printf("Factorial of Number %d is = %d",x,sum);
+
+	return 0;
+}
+
+

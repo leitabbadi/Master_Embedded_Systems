3b7a6f7f7a6df23203e11b28911e21493f298482
Leit Abbadi <158479861+leitabbadi@users.noreply.github.com>
2024-03-09 13:58:41 -0800

    Create EX2 C Program to Check Vowel or Consonant.c

---
+++ Unit_2_C_Programing/2_Loop_&_Condition/EX2 C Program to Check Vowel or Consonant.c
@@ -0,0 +1,30 @@
+/*
+ ===================================Home_work-2=====================================
+ Author      : Leit Abbadi
+ Created on	 : Feb 21, 2024
+ Description : Unit 2 C-Progtamming >> Assignment 2 C-Basic >> EX2 C Program
+               to Test_check Vowel or Consonant using Ansi-style (c89)
+ ===================================================================================
+ */
+#include <stdio.h>
+
+int main() 
+{
+    char Test_ch;
+
+    printf("Enter an Alphabet: ");
+    fflush(stdout);
+    scanf("%c", &Test_ch);
+
+    if (Test_ch == 'a' || Test_ch == 'e' || Test_ch == 'i' || Test_ch == 'o' || Test_ch == 'u' ||
+        Test_ch == 'A' || Test_ch == 'E' || Test_ch == 'I' || Test_ch == 'O' || Test_ch == 'U')
+        printf("%c is a vowel.", Test_ch);
+
+    else if ((Test_ch >= 'a' && Test_ch <= 'z') || (Test_ch >= 'A' && Test_ch <= 'Z'))
+        printf("%c is a consonant.", Test_ch);
+
+    else
+        printf("%c is not a vowel or consonant.", Test_ch);
+
+    return 0;
+}
